24da965fd5a9403ce8a10bbefedd5d7651cf5c24
Abduxoliq <uzoqovabduxoliq@gmail.com>
2025-08-26 00:12:55 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/'
-},
-build: {
-  outDir: 'dist',   // build qilinganda chiqadigan papka
-  emptyOutDir: true, // eski fayllarni tozalab tashlaydi
 })
